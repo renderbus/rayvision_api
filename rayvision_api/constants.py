ceae80f2865cfb33f473f97d1239ed2137b921d8
dan <1113965383@qq.com>
2020-01-10 20:02:04 +0800

    update software name:3dsmax-->3ds Max

--- rayvision_api/constants.py
+++ rayvision_api/constants.py
@@ -7,7 +7,7 @@ PACKAGE_NAME = 'rayvision_api'
 # ``cgId`` from the alias.
 DCC_ID_MAPPINGS = {
     'maya': 2000,
-    '3dsmax': 2001,
+    '3ds Max': 2001,
     'lightwave': 2002,
     'arnold': 2003,
     'houdini': 2004,
@@ -79,15 +79,13 @@ CG_SETTING = {
     'Katana': '2016',
     'Clarisse': '2013',
     'Blender': '2007',
-    '3dsmax': '2001',
+    '3ds Max': '2001',
     '2000': 'Maya',
     '2004': 'Houdini',
     '2016': 'Katana',
     '2013': 'Clarisse',
     '2007': 'Blender',
-    '2001': '3dsmax'
-
-
+    '2001': '3ds Max'
 }
 
 TASK_INFO = {
